7b65a3de95881caebeb46aab8c6b63429639d1aa
abdul-09 <abdul.9.hussein@gmail.com>
2023-10-22 03:49:17 -0700

    updated

--- models/user.py
+++ models/user.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
-
-
-#!/usr/bin/python3
-"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -14,17 +8,13 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    places = relationship('Place', cascade='all, delete, delete-orphan',
+                          backref='user')
+
+    reviews = relationship('Review', cascade='all, delete, delete-orphan',
+                           backref='user')
